02e5c875c2eb829dcb486d9e17db1661f97e88fc
vinceevogue <ferreiravincent115@gmail.com>
2025-02-24 17:13:30 +0100

    done first day

--- src/question.js
+++ src/question.js
@@ -1,7 +1,43 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
+  // 2. shuffleChoices()
+  // ['1', '2' ,'4']
 
-    // 2. shuffleChoices()
+  //['3',]
+  shuffleChoices() {
+    let shuffleChoices = [];
+    let choicesLength = this.choices.length;
+    for (let i = 0; i < choicesLength; i++) {
+      let index = Math.round(Math.random() * (choicesLength - 1));
+      while (shuffleChoices.includes(this.choices[index])) {
+        index = Math.round(Math.random() * (choicesLength - 1));
+      }
+      shuffleChoices.push(this.choices[index]);
+      //   this.choices.slice(index);
+      console.log(this.choices, shuffleChoices);
+    }
+    this.choices = shuffleChoices;
+  }
 }
+const testChoices = [
+  "choice1",
+  "choice2",
+  "choice3",
+  "choice4",
+  "choice5",
+  "choice6",
+  "choice7",
+];
+
+// Instantiate a new Question object with the test choices array
+const question = new Question("test", testChoices, "test");
+// Call the shuffleChoices() method on the Question object
+question.shuffleChoices();
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,59 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 4. shuffleQuestions()
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    // this.questions.forEach((question) => {
+    //   //! question is instance of Question (class) so i can use the method of this class. here i use the metho shuffleChoices inside this object.
+    //   question.shuffleChoices();
+    // });
+    let shuffleQuestions = [];
+    for (let i = 0; i < this.questions.length; i++) {
+      let index = Math.round(Math.random() * (this.questions.length - 1));
 
-    // 5. checkAnswer(answer)
+      while (shuffleQuestions.includes(this.questions[index])) {
+        index = Math.round(Math.random() * (this.questions.length - 1));
+      }
 
-    // 6. hasEnded()
+      shuffleQuestions.push(this.questions[index]);
+      //   console.log(this.choices, shuffleChoices);
+    }
+    this.questions = shuffleQuestions;
+  }
+
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    //!this.question = array of all questions
+    //! this.currenQuestionIndex = the index of the current question
+    //! this.questions[this.currentQuestionIndex] = the current Object question
+    //! because this.questions[this.currentQuestionIndex] its a object i can select all this property with .property  : here .answer
+    //! this.questions[this.currentQuestionIndex].answer = the current correct answer
+    if (answer === this.questions[this.currentQuestionIndex].answer) {
+      this.correctAnswers++;
+    }
+  }
+
+  // 6. hasEnded()
+  hasEnded() {
+    //! this return all the time false except when currentQuestionIndex is equal to the questions array length
+    return this.currentQuestionIndex === this.questions.length;
+  }
 }
